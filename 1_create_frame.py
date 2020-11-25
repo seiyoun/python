8777e1ea03ea55c425ca6c966ff1586a2d6ccfe4
seiyoun <shiyuan0106@gmail.com>
2020-11-25 14:37:44 +0900

    pythone練習

---
+++ 1_create_frame.py
@@ -0,0 +1,8 @@
+from tkinter import *
+
+root = Tk()
+root.title("Sample")
+root.geometry("640x480")
+root.resizable(False,False)
+
+root.mainloop()
---
+++ 2_button.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+root = Tk()
+root.title("Sample")
+root.geometry("640x480")
+root.resizable(False,False)
+
+btn1 = Button(root,text= "button1")
+btn1.pack()
+
+root.mainloop()
